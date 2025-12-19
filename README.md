29e54d9a6a7fac63a8638068a08c2137cbdf8d2d
Samuel <a25samuelgb@iessanclemente.net>
2025-12-19 10:19:07 +0100

    19/12

--- README.md
+++ README.md
@@ -9,9 +9,8 @@ Gestión de videojuego de aventuras y fantasía.
 ## Análisis
 ### Diagrama de clases
 
-[](https://www.mermaidchart.com/app/projects/b381fd1f-e4d7-4dd3-a9f8-66bd298024ad/diagrams/84b3cfed-ebf6-4999-97ab-3c48613ab9c1/version/v0.1/edit)
+[Diagrama](https://www.mermaidchart.com/app/projects/b381fd1f-e4d7-4dd3-a9f8-66bd298024ad/diagrams/84b3cfed-ebf6-4999-97ab-3c48613ab9c1/version/v0.1/edit)
 
----
+### Diseño ER
 
-## Diseño
-### Modelo ER
+[Modelo ER]([[Diagrama](https://www.mermaidchart.com/app/projects/b381fd1f-e4d7-4dd3-a9f8-66bd298024ad/diagrams/84b3cfed-ebf6-4999-97ab-3c48613ab9c1/version/v0.1/edit)](https://drive.google.com/file/d/1Z2CFpKCQrBSWUbgGAY_Htd7tehR82XNd/view?usp=sharing))
--- src/main/java/com/example/Conjuro.java
+++ src/main/java/com/example/Conjuro.java
@@ -2,5 +2,45 @@ package com.example;
 
 public class Conjuro {
 
-    
+    public int id;
+    public Mago mago;
+    public Hechizo hechizo;
+
+    public Conjuro() {}
+
+    public Conjuro(int id, Mago mago, Hechizo hechizo) {
+        this.id = id;
+        this.mago = mago;
+        this.hechizo = hechizo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Mago getMago() {
+        return mago;
+    }
+
+    public Hechizo getHechizo() {
+        return hechizo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMago(Mago mago) {
+        this.mago = mago;
+    }
+
+    public void setHechizo(Hechizo hechizo) {
+        this.hechizo = hechizo;
+    }
+
+    @Override
+    public String toString() {
+        return "Hechizo: " + hechizo.getNombre() + ".\nConjurado por: " + mago.getNombre() + ".";
+    }
+
 }
